05a1241446bce0c26bea118f4e2a2c02f20fee3f
姚才康 <152959150+yck0609@users.noreply.github.com>
2024-02-13 23:47:22 +0800

    0213

--- spzx-service/service-order/src/main/java/mapper/OrderLogMapper.java
+++
@@ -1,9 +0,0 @@
-package mapper;
-
-import com.atguigu.spzx.model.entity.order.OrderLog;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface OrderLogMapper {
-    void save(OrderLog orderLog);
-}
--- spzx-service/service-pay/src/main/java/service/AlipayService.java
+++
@@ -1,5 +0,0 @@
-package service;
-
-public interface AlipayService {
-    String submitAlipay(String orderNo);
-}
