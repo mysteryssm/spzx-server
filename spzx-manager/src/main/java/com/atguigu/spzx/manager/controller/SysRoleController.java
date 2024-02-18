8e55191ac439c64d184062445cfb20d5372e758e
yck0609 <yck0609@126.com>
2024-02-18 22:05:40 +0800

    0218

--- spzx-manager/src/main/java/com/atguigu/spzx/manager/controller/SysRoleController.java
+++ spzx-manager/src/main/java/com/atguigu/spzx/manager/controller/SysRoleController.java
@@ -6,6 +6,8 @@ import com.atguigu.spzx.model.entity.system.SysRole;
 import com.atguigu.spzx.model.globalEnum.ResultCodeEnum;
 import com.atguigu.spzx.model.vo.common.Result;
 import com.github.pagehelper.PageInfo;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
  * @create: 2024-02-18
  */
 
+@Tag(name = "用户角色接口")
 @RestController
 @RequestMapping(value = "admin/system/sysRole")
 public class SysRoleController {
@@ -22,6 +25,7 @@ public class SysRoleController {
     @Autowired
     private SysRoleService sysRoleService;
 
+    @Operation(summary = "分页查询接口")
     @PostMapping(value = "/findByPage/{current}/{limit}")
     public Result<PageInfo<SysRole>> findByPage(@PathVariable("current") Integer current, @PathVariable("limit") Integer limit,
                              @RequestBody SysRoleDto sysRoleDto) {
