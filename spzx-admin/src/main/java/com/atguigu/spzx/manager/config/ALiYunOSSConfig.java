e391580e79da2d5a7559ecdcbfacf3c4ab9611ba
姚才康 <yck0609@126.com>
2024-03-09 21:52:09 +0800

    0309

---
+++ spzx-admin/src/main/java/com/atguigu/spzx/manager/config/ALiYunOSSConfig.java
@@ -0,0 +1,36 @@
+package com.atguigu.spzx.manager.config;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.atguigu.spzx.manager.properties.ALiYunOSSProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+/**
+ * @description: ALiYunOSSConfig
+ * @author: yck
+ * @create: 2024-02-21
+ */
+
+@Component
+public class ALiYunOSSConfig {
+
+    @Autowired
+    private ALiYunOSSProperties aLiYunOSSProperties;
+
+    @Bean
+    public OSS ossClient() {
+        // 创建OSSClient实例
+        OSS ossClient = new OSSClientBuilder().build(aLiYunOSSProperties.getEndpoint(),
+                aLiYunOSSProperties.getAccessKeyID(),
+                aLiYunOSSProperties.getAccessKey());
+
+        // 判断试图存储文件的桶是否存在，若不存在则创建一个新的桶
+        if (!ossClient.doesBucketExist(aLiYunOSSProperties.getBucketName())) {
+            ossClient.createBucket(aLiYunOSSProperties.getBucketName());
+        }
+
+        return ossClient;
+    }
+}
---
+++ spzx-admin/src/main/java/com/atguigu/spzx/manager/controller/RoleController.java
@@ -0,0 +1,74 @@
+package com.atguigu.spzx.manager.controller;
+
+import com.atguigu.spzx.manager.service.SysRoleService;
+import com.atguigu.spzx.model.dto.system.AssignMenuDto;
+import com.atguigu.spzx.model.dto.system.SysRoleDto;
+import com.atguigu.spzx.model.entity.system.SysRole;
+import com.atguigu.spzx.model.globalEnum.ResultCodeEnum;
+import com.atguigu.spzx.model.vo.common.Result;
+import com.github.pagehelper.PageInfo;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+/**
+ * @description: SysRoleController
+ * @author: yck
+ * @create: 2024-02-18
+ */
+
+@Tag(name = "管理员角色管理接口")
+@RestController
+@RequestMapping(value = "admin/role")
+public class RoleController {
+
+    @Autowired
+    private SysRoleService sysRoleService;
+
+    @Operation(summary = "角色添加")
+    @PostMapping(value = "/insert")
+    public Result insert(@RequestBody SysRole sysRole) {
+        sysRoleService.saveSysRole(sysRole);
+        return Result.build(null, ResultCodeEnum.SUCCESS);
+    }
+
+    @Operation(summary = "角色删除")
+    @DeleteMapping(value = "/delete/{roleId}")
+    public Result delete(@PathVariable(value = "roleId") Long roleId) {
+        sysRoleService.deleteSysRole(roleId);
+        return Result.build(null, ResultCodeEnum.SUCCESS);
+    }
+
+    @Operation(summary = "角色修改")
+    @PutMapping(value = "/update")
+    public Result update(@RequestBody SysRole sysRole) {
+        sysRoleService.updateSysRole(sysRole);
+        return Result.build(null, ResultCodeEnum.SUCCESS);
+    }
+
+    @Operation(summary = "指定管理员角色查询")
+    @GetMapping(value = "/select/{administratorId}")
+    public Result<Map<String, Object>> findAllRoles(@PathVariable(value = "administratorId") Long administratorId) {
+        Map<String, Object> map = sysRoleService.findAllRoles(administratorId);  // 包含所有角色以及指定用户具有的角色
+        return Result.build(map, ResultCodeEnum.SUCCESS);
+    }
+
+    @Operation(summary = "角色分页查询")
+    @PostMapping(value = "/select/{page}/{size}")
+    public Result<PageInfo<SysRole>> findByPage(@PathVariable(value = "current") Integer current,
+                                                @PathVariable(value = "limit") Integer limit,
+                                                @RequestBody SysRoleDto sysRoleDto) {
+        PageInfo<SysRole> pageInfo = sysRoleService.findByPage(sysRoleDto, current, limit);
+        return Result.build(pageInfo, ResultCodeEnum.SUCCESS);
+    }
+
+    @Operation(summary = "角色菜单分配")
+    @PostMapping(value = "/menu/assign")
+    public Result assignRole(@RequestBody AssignMenuDto assignMenuDto) {
+        sysRoleService.assignMenu(assignMenuDto);
+        return Result.build(null, ResultCodeEnum.SUCCESS);
+    }
+}
